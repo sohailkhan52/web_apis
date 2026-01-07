5fb5365bea2bf27679ca9922e68257eeda903223
Sohail Khan <sohailkhan52@gmail.com>
2026-01-07 18:16:49 +0500

    Updated routes files

--- routes/transactions.php
+++ routes/transactions.php
@@ -6,21 +6,163 @@
 // --------------------
 require __DIR__ . '/../middleware/auth.php';
 
-switch ($$methods) {
+function  validate($field,$value){
+    if(!isset($value) || trim($value) === ''){
+        http_response_code(422);
+        echo json_encode(['Status'=>false,"Message"=>"$field cannnot be empty"]);exit;
+    }
+}
+
+switch ($methods) {
     case 'GET':
-        # code...
+        $data=$db->select("transactions","*");
+        if(!$data){
+            http_response_code(401);
+            echo json(["Status"=>true,"Message"=>"empty data in the table"]);exit;
+            }
+
+            $displayData=[];
+            foreach ($data as $datum) {
+               $displayData[]=[
+                "id"=>$datum["id"],
+                "user_id"=>$datum["user_id"],
+                "trx_id"=>$datum["trx_id"],
+                "type"=>$datum["type"],
+                "date"=>$datum["date"],
+                "payment_gateway"=>$datum["payment_gateway"],
+                "amount"=>$datum["amount"],
+                "currency"=>$datum["currency"],
+                "description"=>$datum["description"],
+                "attachment"=>$datum["attachment"],
+                "status"=>$datum["status"],
+                "created_by"=>$datum["created_by"],
+                "created_at"=>$datum["created_at"],
+               ];
+            }
+            if(!$displayData){
+                http_response_code(400);
+                echo json_encode(["status"=>false,"Message"=>"data managing error"]);exit;
+            }
+            else{
+                http_response_code(200);
+                echo json_encode(['Status'=>true,"Message"=>"Data fetched Successfully","Data"=>$displayData]);exit;
+            }
+        
         break;
     
     case 'POST':
-        # code...
-        break;
+        $user_id=trim($_POST["user_id"]);
+        validate("user_id",$_POST["user_id"]);
+        if(!is_numeric($user_id)){
+            http_response_code(404);
+            echo json_encode(['status'=>false,"message"=>"User id should be numeric"]);exit;
+        }
+        
+        $trx_id="TRX".date("YmdHis").rand(1000, 9999);
+        $type=strtolower(trim($_POST["type"]));
+        validate("type",$_POST["type"]);
+        if($type!="credit"&&$type!="debit"){
+            http_response_code(404);{
+                echo json_encode(["Status"=>false,"Message"=>"Type can only be credit or debit"]);exit;
+            }
+        }
+        $date=date("Y-m-d H:i:s");
+        $payment_gateway=ucwords($_POST["payment_gateway"]);
+        validate("payment_gateway",$_POST["payment_gateway"]);
+        $amount=$_POST["amount"];
+        validate("amount",$_POST["amount"]);
+        if (!is_numeric($amount) || $amount <= 0) {
+            http_response_code(422);
+            echo json_encode(["status"=>false, "Message"=>"Amount must be a positive number"]);exit;
+         }
+        $currency=strtoupper(trim($_POST["currency"]));
+        validate("currency",$_POST["currency"]);
+        if (strlen($currency) !== 3) {
+            http_response_code(422);
+            echo json_encode(["status"=>false, "Message"=>"Currency must be a 3-letter code"]);exit;            
+         }
+        $description=ucfirst($_POST["description"]);
+        validate("description",$_POST["description"]);
+        if (strlen($description) > 255) {
+            http_response_code(422);
+            echo json_encode(["status"=>false, "Message"=>"Description too long"]);exit; 
+           }
+        $attachment=$_POST["attachment"];
+        validate("attachment",$_POST["attachment"])??NULL;
+        $status=$_POST["status"]??"pending";
+        $created_by=ucwords($_POST["created_by"]);
+        validate("created_by",$_POST["created_by"]);
+        $created_at=date("Y-m-d H:i:s");
     
+        $result=$db->insert("transactions",[
+             "user_id"=>$user_id,
+             "trx_id"=>$trx_id,
+             "type"=>$type,
+             "date"=>$date,
+             "payment_gateway"=>$payment_gateway,
+             "amount"=>$amount,
+             "currency"=>$currency,
+             "description"=>$description,
+             "attachment"=>$attachment,
+             "status"=>$status,
+             "created_by"=>$created_by,
+             "created_at"=>$created_at,
+        ]);
+          if(empty($result)){
+                 http_response_code(401);
+                 echo json_encode(['Status'=>false,"Message"=>"error occure while updating",["trx_id" => $trx_id] ]);exit;              
+            }
+             else{
+                 http_response_code(200);
+                 echo json_encode(['Status'=>true,"Message"=>"status updated successfully","status"=>$status]);exit;       
+             }        
+        break;
     case 'PUT':
-        # code...
+        
+        $input=json_decode(file_get_contents("php://input"),true);
+        $input_id=$input['id'];
+        validate("id",$input['id']);
+        
+        $input_id_exist=$db->get("transactions","*",['id'=>$input_id]);
+        if(empty($input_id_exist)){
+            http_response_code(401);
+            echo json_encode(['Status'=>false,"Message"=>"invalid id"]);exit;
+        }
+        $status=strtolower(trim($input['status']));
+        validate("status",$status);
+        if($status!="success"&&$status!="failed"&& $status!="cancelled"&& $status!="pending"){
+            http_response_code(401);
+            echo json_encode(['Status'=>false,"Message"=>"status must be pending success failed or cancelled"]);exit;           
+        }
+        $result=$db->update("transactions",['status'=>$status],['id'=>$input_id]);
+        if(empty($result)){
+             http_response_code(401);
+            echo json_encode(['Status'=>false,"Message"=>"error occure while updating"]);exit;              
+        }
+        else{
+            http_response_code(200);
+            echo json_encode(['Status'=>true,"Message"=>"status updated successfully","status"=>$status]);exit;       
+        }
         break;
     
     case 'DELETE':
-        # code...
+        $input=json_decode(file_get_contents("php://input"),true);
+        $input_id=$input['id'];
+        validate("id",$input_id);
+        $input_id_exist=$db->get("transactions","*",['id'=>$input_id]);
+        if (empty($input_id_exist) || $input_id_exist['status'] === "cancelled") {
+            http_response_code(400);
+            echo json_encode(['status'=>false,"Message"=>"invalid id or cancelled status "]);exit;
+        }
+        $result=$db->update("transactions",["status"=>"cancelled"],["id"=>$input_id]);
+        if(empty($result)){
+            http_response_code(404);
+            echo json_encode(['status'=>false,"Message"=>"Error occure while transaction deleting "]);exit;
+        }        
+        else{
+            http_response_code(200);
+            echo json_encode(['status'=>true,"Message"=>"transaction deleted successfully"]);exit;
+        }
         break;
     
     default:
--- test.php
+++ test.php
@@ -4,6 +4,14 @@
 202 Accepted - Request accepted for processing
 204 No Content - Successful but no content to return
 
+
+git status
+git add .
+git commit -m "Updated routes files"
+ git push
+git push -u origin main
+
+
 // Client Errors
 400 Bad Request - Invalid request syntax/parameters ✅ Use for: invalid input
 401 Unauthorized - Authentication required ✅ Use for: missing/invalid token
@@ -24,14 +32,21 @@ header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type, Authorization");
 
 
-            "id"=>$datum['id'],
-            "setting_type"=>$datum['setting_type'],
-            "name"=>$datum['name'],
-            "translations"=>$datum['translations'],
-            "status"=>$datum['status'],
-            "created_at"=>$datum['created_at'],
-            "updated_at"=>$datum['updated_at'],
 
 
 
-            $serialized = serialize($data);
+
+function response($statusCode, $status, $message, $data = null) {
+    http_response_code($statusCode);
+    $res = [
+        "status" => $status,
+        "message" => $message
+    ];
+    if ($data !== null) {
+        $res["data"] = $data;
+    }
+    echo json_encode($res);
+    exit;
+}
+
+response(200, true, "Data fetched successfully", $displayData);
